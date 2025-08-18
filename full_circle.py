1a10bd60abce14f60b9ddb3b4d0f62842aaffdbd
Sheng <demonicplayer404@gmail.com>
2025-08-18 14:30:52 -0400

    true function

---
+++ full_circle.py
@@ -0,0 +1,7 @@
+from math import pi
+
+def calculate_circle_area(radius):
+    area = pi * (radius ** 2)
+    return area
+
+print("The area of a circle with radius 5 is:", calculate_circle_area(5))
